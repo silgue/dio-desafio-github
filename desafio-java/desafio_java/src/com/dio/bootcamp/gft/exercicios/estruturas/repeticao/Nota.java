d52f6a16ca94880c8cb5f83a3300ead09d46b74c
Alysson Rodrigues <rodrigues.alysson@gmail.com>
2022-02-03 12:52:52 -0300

    correção para informar que a nota é inválida

--- desafio-java/desafio_java/src/com/dio/bootcamp/gft/exercicios/estruturas/repeticao/Nota.java
+++ desafio-java/desafio_java/src/com/dio/bootcamp/gft/exercicios/estruturas/repeticao/Nota.java
@@ -14,11 +14,12 @@ public class Nota {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe uma nota entre 0 e 10: ");
 		
-		int nota = 11;
+		int nota = scan.nextInt();
 			
 		while(nota < 0 || nota > 10) {
-			System.out.println("Informa uma nota entre 0 e 10: ");
+			System.out.println("Nota inválida! Favor escolha entre os valores 0 e 10: ");
 			
 			nota = scan.nextInt();
 		}
