cadb86305dba5574256f9d445a9ee9172ec7e6a6
Alysson Rodrigues <rodrigues.alysson@gmail.com>
2022-02-03 13:37:18 -0300

    refatoração

--- desafio-java/desafio_java/src/com/dio/bootcamp/gft/exercicios/estruturas/repeticao/Tabuada.java
+++ desafio-java/desafio_java/src/com/dio/bootcamp/gft/exercicios/estruturas/repeticao/Tabuada.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Tabuada {
 
 	public static void main(String[] args) {
-		 Scanner scan = new Scanner(System.in);
-		 
-		 System.out.println("Informe um número de 0 a 10 para ver a tabuada desse número: ");
-		 int numero = scan.nextInt();
-		 
-		 for(int i=1;i<11;i++) {
-			 System.out.println(numero + " x " + i + "= " + numero*i);
-		 }
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Informe um número de 0 a 10 para ver a tabuada desse número: ");
+		int numero = scan.nextInt();
+		System.out.println("Tabuada de " + numero + ":");
+		for (int i = 1; i < 11; i++) {
+			System.out.println(numero + " x " + i + " = " + numero * i);
+		}
 
 	}
 
